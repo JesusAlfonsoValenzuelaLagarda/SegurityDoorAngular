c5ac3b7fb1b48b240ca82a8043106769b768dcf6
HanselEsp <69163352+HanselEsp@users.noreply.github.com>
2021-04-22 02:01:37 -0700

    añadi path de createusercontrol

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AddAccessControlComponent } from './components/add-access-control/add-access-control.component';
 import { AdminComponent } from './components/admin/admin.component';
+import { CreateUserControlComponent } from './components/create-user-control/create-user-control.component';
 
 const routes: Routes = [
   {
@@ -12,6 +13,9 @@ const routes: Routes = [
   },
   {
     path: "Admin", component: AdminComponent
+  },
+  {
+    path: "CreateUserControl", component: CreateUserControlComponent
   }
 
 ];
